5a4d8dff99e62082f2b23a0516d441863d32e1f0
AminulSagor <aminulislamsagor3@gmail.com>
2025-05-03 08:52:18 +0600

    Added nothing

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -15,22 +15,18 @@ export class TwilioService {
 
   async makeCall(to: string, audioUrl: string): Promise<any> {
     const from = this.config.get<string>('TWILIO_PHONE_NUMBER');
-    const baseUrl = this.config.get<string>('NGROK_BASE_URL'); // ✅ from .env
+  
 
     if (!from) {
       throw new Error('Twilio FROM number is not defined in env');
     }
 
-    if (!baseUrl) {
-      throw new Error('NGROK_BASE_URL is not defined in env');
-    }
-
-    const twimlUrl = `${baseUrl}/twiml/call/${encodeURIComponent(audioUrl)}`;
+  
 
     const response = await this.client.calls.create({
       to,
       from,
-      url: twimlUrl,
+      url: audioUrl,
     });
 
     return response;
